a3da764ac58f328c20a5e064b00e0aea3ea24545
EdgarRomero2000 <eromero22@jaumebalmes.net>
2023-01-18 20:37:04 +0100

    Practica09_B

---
+++ B/conc.py
@@ -0,0 +1,6 @@
+# Imprimir el resultat de la suma de les dues variables (int)
+int1 = 22
+int2 = 47
+print(int1+int2)
+# Imprimir el resultat de la suma de les dues variables (int) dintre d'un text (string)
+print(f'Hola em dic edgar i m\'agrada el numero {int1+int2}.')
